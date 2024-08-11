c9d78448ed52386a8d778e1167f0df9fffc74db6
Sujit K M <kmsujit@gmail.com>
2024-08-11 09:14:27 +0530

    Integrated Operations in Learner Kit
    Integrated Operations in Learner Kit

--- learnerkit.go
+++ learnerkit.go
@@ -8,6 +8,7 @@ type LearnerKit struct {
 	node    LearnerKitNode
 	graph   LearnerKitGraph
 	machine LearnerKitMachine
+	op      LearnerKitOps
 }
 
 type LearnerKitNode struct {
@@ -21,8 +22,19 @@ type LearnerKitGraph struct {
 type LearnerKitMachine struct {
 	machine VM
 }
+type LearnerKitOps struct {
+	ops Op
+}
 
-func Initialize_LearnerKit(graph *ExprGraph, machine VM, node_v **Node, size int) LearnerKit {
+func ApplyOps_LearnerKit(op Op, children ...*Node) {
+	ApplyOp(op, children...)
+}
+
+func ApplyOps_With_Name_LearnerKit(op Op, name string, children ...*Node) {
+	ApplyOpWithName(op, name, children...)
+}
+
+func Initialize_LearnerKit(graph *ExprGraph, machine VM, node_v **Node, size int, op Op) LearnerKit {
 	learnerkitnode := LearnerKitNode{
 		size: size,
 		node: node_v,
@@ -33,11 +45,14 @@ func Initialize_LearnerKit(graph *ExprGraph, machine VM, node_v **Node, size int
 	learnerkitmachine := LearnerKitMachine{
 		machine: machine,
 	}
-
+	learnerkitops := LearnerKitOps{
+		ops: op,
+	}
 	learnerkit := LearnerKit{
 		node:    learnerkitnode,
 		graph:   learnerkitgraph,
 		machine: learnerkitmachine,
+		op:      learnerkitops,
 	}
 
 	return learnerkit
