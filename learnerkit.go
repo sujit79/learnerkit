a1be2e66eae32e534f2c59cd7aab864973884b1d
Sujit K M <kmsujit@gmail.com>
2024-08-11 12:52:20 +0530

    apply model with name
    apply model with name

--- learnerkit.go
+++ learnerkit.go
@@ -56,6 +56,23 @@ func ApplyOps_LearnerKitModel(model LearnerKitModel) ([][]Node, error) {
 	return results, e
 }
 
+func ApplyOps_WithName_LearnerKitModel_WithName(model LearnerKitModel, name string) ([][]Node, error) {
+	results := make([][]Node, len(model.model_kit))
+	var e error
+	for i := 0; i < len(model.model_kit); i++ {
+		result := make([]Node, len(*model.model_kit[i].node.node))
+		for j := 0; j < len(*model.model_kit[i].node.node); j++ {
+			value, e := ApplyOps_With_Name_LearnerKit(model.model_kit[i].op.ops, name, *model.model_kit[j].node.node...)
+			if e != nil {
+				break
+			}
+			result = append(result, *value)
+		}
+		results[i] = result
+	}
+	return results, e
+}
+
 func Initialize_LearnerKit(graph *ExprGraph, machine VM, op Op, node_v ...*Node) LearnerKit {
 	learnerkitnode := LearnerKitNode{
 		node: &node_v,
